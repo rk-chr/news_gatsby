f0b4e3a06f8261c57bdf10194d6db0b934fd34f5
kishore2486 <ramkishore157359@gmail.com>
2019-07-07 03:57:56 +0530

    changed api for depolyment

--- axios.js
+++ axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:3002'
+  baseURL: 'https://comments.bandwidth.co.in'
 });
 
 instance.interceptors.request.use(
