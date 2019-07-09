1638fecc36fd9e38520688c2aecfb0ba69ca234e
kishore2486 <ramkishore157359@gmail.com>
2019-07-09 15:43:05 +0530

    changed live api url

--- axios.js
+++ axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'https://comments.bandwidth.co.in'
+  baseURL: 'https://comments.bingewatch.co.in'
 });
 
 instance.interceptors.request.use(
